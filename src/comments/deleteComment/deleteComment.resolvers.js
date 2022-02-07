26570b0d03dcb41d9380e2d1d7f68abe362cc57c
최성준 <db970406@naver.com>
2022-02-07 20:48:20 +0900

    feat: Add deleteComment 구현
    1. 무엇을? : 댓글 삭제 기능
    2. 왜? : 댓글을 삭제하기 위함

--- src/comments/deleteComment/deleteComment.resolvers.js
+++ src/comments/deleteComment/deleteComment.resolvers.js
@@ -8,10 +8,9 @@ import { checkLoginState } from '../../users/users.utils';
 
 export default {
     Mutation: {
-        editComment: checkLoginState(
-            async (_, { commentId, comment }, { loggedInUser }) => {
+        deleteComment: checkLoginState(
+            async (_, { commentId }, { loggedInUser }) => {
                 try {
-
                     // 존재하는 댓글인지 체크하고, 있는 경우 connect되어 있는 userId만 가져온다
                     const isExistComment = await client.comment.findUnique({
                         where: {
@@ -23,22 +22,19 @@ export default {
                     });
                     if (!isExistComment) throw new Error("존재하지 않는 댓글입니다.");
 
-                    // 댓글이 있다면 위에서 가져온 userId로 수정권한이 있는지 체크
+                    // 게시글이 있다면 위에서 가져온 userId로 삭제권한이 있는지 체크
                     else if (isExistComment.userId !== loggedInUser.id) throw new Error("댓글의 주인이 아닙니다.");
                     else {
-                        await client.comment.update({
+                        await client.comment.delete({
                             where: {
                                 id: commentId
-                            },
-                            data: {
-                                comment
                             }
                         });
 
                         return {
                             ok: true
-                        }
-                    }
+                        };
+                    };
 
                 } catch (error) {
                     return {

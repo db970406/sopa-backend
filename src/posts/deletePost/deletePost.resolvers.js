46693c46161ff1ed75c9318741c2ea63956c531d
최성준 <db970406@naver.com>
2022-02-07 16:19:59 +0900

    feat: Add deletePost 구현
    1. 무엇을? : 게시글 삭제 기능 구현
    2. 왜? : 게시글 데이터 CRUD중 D 구현

--- src/posts/deletePost/deletePost.resolvers.js
+++ src/posts/deletePost/deletePost.resolvers.js
@@ -8,8 +8,8 @@ import { checkLoginState } from '../../users/users.utils';
 
 export default {
     Mutation: {
-        editPost: checkLoginState(
-            async (_, { postId, title, description }, { loggedInUser }) => {
+        deletePost: checkLoginState(
+            async (_, { postId }, { loggedInUser }) => {
                 try {
                     // 존재하는 게시글인지 체크하고, 있는 경우 connect되어 있는 userId만 가져온다
                     const isExistPost = await client.post.findUnique({
@@ -22,16 +22,12 @@ export default {
                     })
                     if (!isExistPost) throw new Error("존재하지 않는 게시글입니다.")
 
-                    // 게시글이 있다면 위에서 가져온 userId로 수정권한이 있는지 체크
+                    // 게시글이 있다면 위에서 가져온 userId로 삭제권한이 있는지 체크
                     else if (isExistPost.userId !== loggedInUser.id) throw new Error("게시글의 주인이 아닙니다.");
                     else {
-                        await client.post.update({
+                        await client.post.delete({
                             where: {
                                 id: postId
-                            },
-                            data: {
-                                title,
-                                description
                             }
                         })
 

4d92ffa64c17bd6f24f460e91d65bcfcb5f218a4
최성준 <db970406@naver.com>
2022-02-27 15:13:43 +0900

    feat: Add seePostsCount 추가, seePosts 정렬을 백엔드에서 하도록 수정
    1. 무엇을? :
    - 요청받은 데이터의 전체 길이를 구해주는 seePostsCount 추가
    - seePosts에서 정렬 방식을 받아 데이터를 구하여 return 하게 함
    2. 왜? :
    - 데이터의 최종 길이를 알려주어 프론트에서 pagination offset에 활용하여 마지막 데이터에 닿은 경우 더이상 fetchMore이 일어나지 않게 하기 위함
    - 프론트에서 정렬하려고 하면 pagination offset 때문에 프론트가 받은 데이터 내에서만 정렬을 할 수 있기에 사실상 효과가 없었다.
    - 따라서 DB에 있는 모든 데이터 중에서 정렬하여 가져올 수 있게 백엔드에서 정렬하는 것이 낫다고 판단하여 수정함

--- src/apollo/posts/seePosts/seePosts.resolvers.js
+++ src/apollo/posts/seePosts/seePosts.resolvers.js
@@ -1,6 +1,6 @@
 /**
  * 생성일 : 22.02.07
- * 수정일 : 22.02.26
+ * 수정일 : 22.02.27
  */
 
 import client from '../../../client';
@@ -8,11 +8,36 @@ import { sortSkillsbyPosition } from '../posts.utils';
 
 export default {
     Query: {
-        seePosts: async (_, { offset, skills }) => {
+        seePosts: async (_, { offset, skills, howToArrangement = "new" }) => {
             // Home에 게시글들을 나열할 때 offset을 활용한다(page식을 쓸 것인지는 나중에 생각해보자)
             let resultsArray = [];
             if (skills) {
                 resultsArray = sortSkillsbyPosition(skills)
+            }
+
+            const makeArrangement = () => {
+                switch (howToArrangement) {
+                    case "new":
+                        return {
+                            createdAt: "desc"
+                        }
+                    case "likeCount":
+                        return {
+                            likes: {
+                                _count: "desc"
+                            },
+                        }
+                    case "commentCount":
+                        return {
+                            comments: {
+                                _count: "desc"
+                            },
+                        }
+                    case "readCount":
+                        return {
+                            readCount: "desc"
+                        }
+                }
             }
 
             const posts = await client.post.findMany({
@@ -25,10 +50,9 @@ export default {
                 },
                 take: 6,
                 skip: offset,
-                orderBy: {
-                    createdAt: "desc"
-                }
+                orderBy: makeArrangement()
             })
+
             return posts
         }
     }
--- src/apollo/posts/seePosts/seePosts.typeDefs.js
+++ src/apollo/posts/seePosts/seePosts.typeDefs.js
@@ -1,12 +1,12 @@
 /**
  * 생성일 : 22.02.07
- * 수정일 : 22.02.26
+ * 수정일 : 22.02.27
  */
 
 import { gql } from "apollo-server";
 
 export default gql`
     type Query{
-        seePosts(offset:Int,skills:String):[Post]
+        seePosts(offset:Int,skills:String,howToArrangement:String):[Post]
     }
 `

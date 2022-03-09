0823212c3e9226c224aaa4110e0408ff1d2c88c7
최성준 <db970406@naver.com>
2022-03-09 22:13:31 +0900

    feat: Add User모델 githubURL 필드 추가 및 editUser 수정
    1. 무엇을? : User모델에 githubURL을 추가하고 editUser resolver를 통해 수정할 수 있게함
    2. 왜? : 글 작성자의 깃허브를 확인할 수 있게 하기 위함

--- src/apollo/users/editUser/editUser.typeDefs.js
+++ src/apollo/users/editUser/editUser.typeDefs.js
@@ -1,12 +1,12 @@
 /**
  * 생성일 : 22.02.07
- * 수정일 : ------
+ * 수정일 : 22.03.09
  */
 
 import { gql } from 'apollo-server';
 
 export default gql`
     type Mutation{
-        editUser(name:String,password:String):MutationResults!
+        editUser(name:String,password:String,githubURL:String):MutationResults!
     }
 `
